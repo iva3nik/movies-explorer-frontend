1b4960b546d2d8f8fa37fd72dfa78d23763f643f
Ivan Nikitin <iva3nik@yandex.ru>
2021-10-05 19:03:57 +0300

    feat: A button of removal for card has been created

--- src/components/MoviesCard/MovieCard.js
+++ src/components/MoviesCard/MovieCard.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './MovieCard.css';
 import picture from '../../images/somepicture.jpg'
+import { Route } from 'react-router-dom';
 
 function MovieCard() {
   return (
@@ -8,7 +9,18 @@ function MovieCard() {
       <img className='movie-card__poster' src={picture} alt='Обложка фильма' />
       <div className='movie-card__strip'>
         <h3 className='movie-card__name'>33 words about design</h3>
-        <button className='movie-card__button-like' />
+        <Route path='/movies'>
+          <button
+            className='movie-card__button-like'
+            type='button'
+          />
+        </Route>
+        <Route path='/saved-movies'>
+          <button
+            className='movie-card__button-delete'
+            type='button'
+          />
+        </Route>
       </div>
       <div className='movie-card__duration'>12312</div>
     </div>
